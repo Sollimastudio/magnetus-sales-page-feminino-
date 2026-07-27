17191cf54a03fa6ed19e69e74a04243f9d4839a1
Sollimastudio <sollimalovecoach@gmail.com>
2026-07-27 17:40:45 -0300

    feat: ativa continuidade dos criativos na página feminina

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ConversionContinuityLayer from './components/ConversionContinuityLayer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <ConversionContinuityLayer />
   </StrictMode>,
 )
